ccff7ce5ae43a2db7e5068fd1497b4696f514810
Reshu gupta <124056347+Reshugupta2003@users.noreply.github.com>
2024-04-24 17:08:18 +0530

    Sorting

--- Selection.java
+++ Selection.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Selection {
 
     public static void selectionSort(int arr[], int size){
+
+      int Count =0;
          for(int i=0; i<size-1; i++){
             int mindex = i;
             for(int j=i+1; j<size; j++){
@@ -17,16 +19,20 @@ public class Selection {
                 int temp = arr[i];
                 arr[i] = arr[mindex];
                 arr[mindex] = temp;
+                
+                Count++;
             }
          }
          System.out.println(Arrays.toString(arr));
+         System.out.println("Number of Switching: "+Count);
     }
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
 
        System.out.println("Enter the size of array");
        int size = sc.nextInt();
-        
+       sc.close();
+       
        int arr[] = new int[size];
        System.out.println("Enter the array");
 
--- bubble.java
+++ bubble.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class bubble {
     public static void bubbleSort(int arr[], int size)
     {
+        int count =0;
       for(int i =0; i<size; i++){
         // j< size -1 -i , use 1 and i to avoid indexbound error
         for(int j =0; j<size-i-1; j++){
@@ -15,17 +16,21 @@ public class bubble {
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                
+                count++;
             }
         }              // This is a  program to print array in decreasing order using bubble sort
-      }
+      }                // And counting , how many number are switch for sorting
       System.out.println(Arrays.toString(arr));
+      System.out.println(count);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the size of array");
         int n = sc.nextInt();
-
+        sc.close();
+        
         int arr[] = new int[n];
         System.out.println("Enter the array");
         for(int i =0; i<n; i++){
